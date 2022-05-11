91d95ce1e0617a107c8218c066a011b898dc13ca
VarunVerma7 <varunverma855@gmail.com>
2022-05-10 20:01:40 -0400

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-
+import Second from "./components/second-section";
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <br />
+      <Second />
     </>
   );
 };
